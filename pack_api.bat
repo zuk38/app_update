e5286ba1360ba1be7dd1132911cff341ff586961
Patrycjusz Laskowski <patrycjusz.laskowski@macrix.pl>
2020-10-02 11:18:27 +0200

    Update

---
+++ pack_api.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z" a -pJDIEKNchsjaie api.zip ./api
+git add .
+git commit -m "Update"
+git push origin master
---
+++ pack_app.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z" a -pJDIEKNchsjaie app.zip ./app
+git add .
+git commit -m "Update"
+git push origin master
